c1ac9a9387134d72438013bbb969b776533b924a
yashpratapkushwaha966 <yashkushwaha485005@gmail.com>
2026-07-03 09:42:18 +0530

    update resume

--- portfolio-frontend/src/components/sections/Projects/projectsData.js
+++ portfolio-frontend/src/components/sections/Projects/projectsData.js
@@ -1,13 +1,14 @@
 const projects = [
   {
     id: 1,
-    title: "VotingPage",
+    title: "BloodLink",
     description:
-      "A secure MERN Stack voting platform with user authentication, one-person-one-vote functionality, admin dashboard, and live election results. Designed with a responsive UI for a smooth voting experience.",
+      "A blood donor management platform that helps users quickly find nearby blood donors based on blood group and location, with donor registration and hospital support features.",
     tech: ["React + Node.js + Express + MongoDB"],
-    github: "https://github.com/yashpratapkushwaha966/Voting_page-.git",
-    live: "https://votingpage-theta.vercel.app/"
+    github: "https://github.com/yashpratapkushwaha966/projectbld.git",
+    live: "https://projectbld.vercel.app/ "
   },
+
 
   {
     id: 2,
@@ -21,35 +22,15 @@ const projects = [
 
   {
     id: 3,
-    title: "Mentor Portfolio",
-    description:
-      "A modern personal portfolio website built for a mentor, featuring smooth animations, responsive layouts, service sections, achievements, and an integrated contact form.",
-    tech: ["React + Node.js + Express + MongoDB"],
-    github: "https://github.com/yashpratapkushwaha966/portfolio_website.git",
-    live: "https://madamdev.vercel.app/"
-  },
-
-  {
-    id: 4,
-    title: "BloodLink",
-    description:
-      "A blood donor management platform that helps users quickly find nearby blood donors based on blood group and location, with donor registration and hospital support features.",
-    tech: ["React + Node.js + Express + MongoDB"],
-    github: "https://github.com/yashpratapkushwaha966/projectbld.git",
-    live: "https://projectbld.vercel.app/ "
-  },
-
-  {
-    id: 5,
     title: "Premium Textile",
     description:
-      "A premium business website for a textile brand showcasing collections, company information, responsive design, modern UI, and an elegant user experience for customers.",
+      "A premium business webpage for a textile brand showcasing collections, company information, responsive design, modern UI, and an elegant user experience for customers.",
     tech: ["React + Framer Motion + CSS"],
     github: "https://github.com/yashpratapkushwaha966/premium-textile.git",
     live: "https://premium-textile-zeta.vercel.app/"
   },
   {
-    id: 6,
+    id: 4,
     title: "SGSU Page",
     description:
       "A modern and responsive webpage for SGSU, featuring smooth animations, interactive sections, and a user-friendly interface to provide information about the university and its offerings.",
@@ -57,14 +38,6 @@ const projects = [
     github: "https://github.com/yashpratapkushwaha966/SGSU_webpage.git",
     live: "https://sgsu-webpage.vercel.app/"
   },
-  {
-    id: 7,
-    title: "AI Image Search Bar",
-    description:
-      "An AI-powered image search bar that allows users to find relevant images based on text queries, with a modern UI and seamless integration.",
-    tech: ["HTML + CSS + JavaScript + AI API"],
-    github: "https://github.com/yashpratapkushwaha966/AI-image-search-bar.git",
-  }
 ];
 
 export default projects;
